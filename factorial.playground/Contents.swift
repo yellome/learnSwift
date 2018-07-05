c397e775c7f3e1c934ab8bb3539902ec5a56988c
Allison <allison@ANDREWs-MacBook-Pro-2.local>
2018-07-04 21:43:29 -0700

    updated comments

--- factorial.playground/Contents.swift
+++ factorial.playground/Contents.swift
@@ -1,5 +1,4 @@
-//write a function that computes factorial recursively. use input type of integer and return type of double.
-
+//compute factorial recursively
 func factorial(number: Int) -> Double {
     if number > 1 {
         return Double(number) * factorial(number: number - 1)
